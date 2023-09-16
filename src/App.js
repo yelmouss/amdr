35b9af0cfe9a12954a900ec1371d45dce61cef55
yelmouss <yassine.elmouss@sfr.fr>
2023-09-16 18:15:29 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Site web d'AMDR fondation en cours de construction
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://yelmouss.vercel.app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          yelmouss
         </a>
       </header>
     </div>
